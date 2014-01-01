b01544150870901f398879858b95206889c789c6
Drunkard Zhang <gongfan193@gmail.com>
2014-01-02 01:33:49 +0800

    lazycat: simplify codes
    Drop a lot unnecessary try-except sentences.

--- cli/__init__.py
+++ cli/__init__.py
@@ -1,1 +1,81 @@
 # -*- coding: utf-8 -*-
+
+
+# These commands' output to ncurses like window, which is hard to strip
+OMIT_OUTPUT = ['atop', 'htop', 'iotop', 'iftop', 'dnstop']
+
+
+# Command enumerate
+nolog_cmd = [
+    # {'KEY': ('showed cmd', 'real cmd, maybe system cmd', 'Description')},
+    # Single module functions
+    'clear',
+    'config',
+    'help',
+    'log',
+    'password',
+    'quit',
+    'show',
+    # These tools mapped to system commands
+    'httping',
+    'ping',
+    'ping6',
+    'tcp-ping',
+    'udp-ping',
+    'traceroute',
+    'traceroute6',
+    'tcp-traceroute',
+    'udp-traceroute']
+log_cmd = [
+    'ssh',
+    'telnet']
+all_cmd = log_cmd + nolog_cmd
+
+
+l1_map = [
+    # ('CMD name', 'Real Linux CMD', 'Description'),
+    ('password', 'passwd', 'Change password'),
+    ('time', 'cal -3; date', 'Show local date and time'),
+    # hping need root privilege
+    # ('tcp-ping', 'hping -p 80 --syn ',
+    #  'Ping using TCP protocol, default 80 port'),
+    # ('udp-ping', 'hping --udp -p 53 ',
+    #  'Ping using UDP protocol, default 53 port')
+    ('tcp-ping', 'nping --tcp-connect -p 80 ',
+     'Ping using TCP protocol, using port 80'),
+    ('udp-ping', 'nping --udp -p 53 ',
+     'Ping using UDP protocol, using port 53'),
+    ('tcp-traceroute', 'tcptraceroute ',
+     'traceroute using TCP protocol, using port 80'),
+    # ('tcp-traceroute', 'traceroute -M tcp -p 80 ',
+    #  'traceroute using TCP protocol, using port 80'),
+    ('udp-traceroute', 'traceroute -U -p 53 ',
+     'traceroute using UDP protocol, using port 53')]
+
+
+# Level 2 commands definations
+log_l2 = [
+    'list',
+    'view']
+show_l2 = [
+    'all-jumper',
+    'history',
+    'my-permission',
+    'user',
+    'this-server',
+    'time']
+
+
+# Partial command definations
+clear_comp = ['cl', 'cle', 'clear']
+help_comp = ['he', 'hel', 'help']
+log_comp = ['l', 'lo', 'log']
+log_list_comp = ['l', 'li', 'lis', 'list']
+log_view_comp = ['v', 'vi', 'vie', 'view']
+quit_comp = ['q', 'qu', 'qui', 'quit']
+show_comp = ['sh', 'sho', 'show']
+show_history_comp = ['h', 'history']
+show_my_permission_comp = ['m', 'my', 'my-' 'my-p', 'my-permission']
+show_user_comp = ['u', 'us', 'use', 'user']
+show_this_server_comp = ['th', 'this-server']
+show_time_comp = ['ti', 'tim', 'time']
--- lazycat.py
+++ lazycat.py
@@ -10,7 +10,6 @@ import string
 import sys
 import threading
 import time
-from socket import gethostname
 
 import signal
 import fcntl
@@ -18,10 +17,8 @@ import termios
 import struct
 
 try:
-    # import ANSI
     import pexpect
-    # import rlcompleter2
-    # import IPython.core.completer as completer
+    import cli
     from cli import history
     from lib import cmd_exists, color, say
 except ImportError, e:
@@ -30,115 +27,18 @@ except ImportError, e:
 A critical module was not found. Probably this operating system does not
 support it. Pexpect is intended for UNIX-like operating systems.\033[0m""")
 
+
 __author__ = "Drunkard Zhang"
 __email__ = "gongfan193@gmail.com"
 __version__ = "0.1"
 __productname__ = "lazycat"
 __description__ = "A pseudo shell with restricted capability for AAA purpose."
 
+
 PROMPT = color.GREEN_BOLD + "jumper" + color.OFF + "> "
-SHOW_WARN = 0
-
-# Command enumerate
-nolog_cmd = [
-    # {'KEY': ('showed cmd', 'real cmd, maybe system cmd', 'Description')},
-    # Single module functions
-    'clear',
-    'config',
-    'help',
-    'log',
-    'password',
-    'quit',
-    'show',
-    # These tools mapped to system commands
-    'httping',
-    'ping',
-    'ping6',
-    'tcp-ping',
-    'udp-ping',
-    'traceroute',
-    'traceroute6',
-    'tcp-traceroute',
-    'udp-traceroute']
-log_cmd = [
-    'ssh',
-    'telnet']
-all_cmd = log_cmd + nolog_cmd
-
-# Level 2 commands definations
-config_l2 = [
-    'user',
-    'password',
-    'permission',
-    'tui']
-log_l2 = [
-    'list',
-    'search',
-    'view',
-    'del']
-show_l2 = [
-    'all-jumper',
-    'history',
-    'my-permission',
-    'user',
-    'this-server',
-    'time']
-
-# Level 3 commands definations
-log_search_l3 = [
-    'by-date',
-    'by-time',
-    'by-device-ip',
-    'by-device-name']
-
-# These commands' output to ncurses like window, which is hard to strip
-OMIT_OUTPUT = ['atop', 'htop', 'iotop', 'iftop', 'dnstop']
-
-# Partial command definations
-clear_comp = ['cl', 'cle', 'clear']
-help_comp = ['he', 'hel', 'help']
-log_comp = ['l', 'lo', 'log']
-log_del_comp = ['d', 'de', 'del']
-log_list_comp = ['l', 'li', 'lis', 'list']
-log_view_comp = ['v', 'vi', 'vie', 'view']
-quit_comp = ['q', 'qu', 'qui', 'quit']
-show_comp = ['sh', 'sho', 'show']
-show_history_comp = ['h', 'history']
-show_my_permission_comp = ['m', 'my', 'my-' 'my-p', 'my-permission']
-show_user_comp = ['u', 'us', 'use', 'user']
-show_this_server_comp = ['th', 'this-server']
-show_time_comp = ['ti', 'tim', 'time']
-
-map_resolver = {'name': 0, 'cmd': 1, 'desc': 2}
-l1_map = [
-    # ('CMD name', 'Real Linux CMD', 'Description'),
-    ('password', 'passwd', 'Change password'),
-    ('time', 'cal -3; date', 'Show local date and time'),
-    # hping need root privilege
-    # ('tcp-ping', 'hping -p 80 --syn ',
-    #  'Ping using TCP protocol, default 80 port'),
-    # ('udp-ping', 'hping --udp -p 53 ',
-    #  'Ping using UDP protocol, default 53 port')
-    ('tcp-ping', 'nping --tcp-connect -p 80 ',
-     'Ping using TCP protocol, using port 80'),
-    ('udp-ping', 'nping --udp -p 53 ',
-     'Ping using UDP protocol, using port 53'),
-    ('tcp-traceroute', 'tcptraceroute ',
-     'traceroute using TCP protocol, using port 80'),
-    # ('tcp-traceroute', 'traceroute -M tcp -p 80 ',
-    #  'traceroute using TCP protocol, using port 80'),
-    ('udp-traceroute', 'traceroute -U -p 53 ',
-     'traceroute using UDP protocol, using port 53')]
 
 flush_interval = 10
-title = os.getlogin() + "@" + gethostname()
-del gethostname
 global_pexpect_instance = None  # Used by signal handler
-
-
-def exit_with_usage():
-    print globals()['__doc__']
-    os._exit(1)
 
 
 class MLCompleter(object):  # Custom completer
@@ -183,6 +83,8 @@ class bgrun(threading.Thread):
 
 
 def flushlog():
+    from socket import gethostname
+    title = os.getlogin() + "@" + gethostname()
     while True:
         ts = '\n### %4d-%02d-%02dT%02d:%02d:%02d ' % time.localtime()[:-3]
         try:
@@ -198,14 +100,11 @@ def flushlog():
             return 127
 
 
-def str_to_class(s):
-    import types
-    if s in globals() and isinstance(globals()[s], types.ClassType):
-        return globals()[s]
-    return None
-
-
 def run_with_log(command):
+    # Check if command exists
+    if not cmd_exists(command.split()[0]):
+        say.nocmd(command.split()[0])
+        return False
     # Begin log with timestamp
     from cli.oplog import get_log_file
     global fout
@@ -214,26 +113,17 @@ def run_with_log(command):
     bgrun(flushlog).start()
     # Start pexpect session
     try:
-        if cmd_exists(command.split()[0]):
-            pass
-        else:
-            say.nocmd(command.split()[0])
-            return False
-
         thissession = pexpect.spawn('bash', ['-c', command])
     except pexpect.ExceptionPexpect, e:
         raise e
-        return 1
     except SystemExit, e:
         print(str(e))
-        return 1
+        return False
     except (KeyboardInterrupt, pexpect.EOF, pexpect.TIMEOUT):
-    # except (BaseException, KeyboardInterrupt, EOFError, SystemExit):
-        # thissession.sendcontrol('c')
-        return 1
+        return False
     except BaseException, e:
         print(str(e))
-        return 1
+        return False
 
     thissession.logfile = fout
     global global_pexpect_instance
@@ -242,14 +132,7 @@ def run_with_log(command):
 
     fout.write("### First command: %s\n" % command)
     try:
-        if SHOW_WARN == 1:
-            print ("%sOperation logging started, have fun :)%s" %
-                   (color.RED, color.OFF))
-    except NameError:
-        pass
-    try:
         # TODO: rewrite, control on every cmd
-        # thissession.interact(chr(29))
         thissession.interact()
     except OSError:
         pass
@@ -257,26 +140,18 @@ def run_with_log(command):
         print("\r" + str(e))
         return 1
 
-    try:
-        if SHOW_WARN == 1:
-            print ("%sOperation logging stopped%s" % (color.RED, color.OFF))
-    except NameError:
-        pass
     fout.close()
-    return 0
+    return True
 
 
 def run_without_log(command):
-    command_name = command.strip().split()[0]
-    command_args = command.strip().split()[1:]
-
+    command = command.strip()
+    command_name = command.split()[0]
+    command_args = command.split()[1:]
     # First, try to lookup command map
     FOUND_IN_MAP = False
-    for entry in l1_map:
+    for entry in cli.l1_map:
         if command_name == entry[0]:
-            # debug
-            # print type(entry[1])
-            # print type(command_args)
             command = entry[1] + ' '.join(command_args)
             FOUND_IN_MAP = True
             break
@@ -284,9 +159,7 @@ def run_without_log(command):
             continue
 
     # Second, test if it's in PATH, return if not
-    if cmd_exists(command_name):
-        pass    # goto try: part
-    elif FOUND_IN_MAP is True:
+    if cmd_exists(command_name) or FOUND_IN_MAP:
         pass
     else:
         print("This function is not usable, please recheck PATH or l1_map: %s"
@@ -313,27 +186,23 @@ def do_log(command):
     sub.reverse()
     # If no sub-command, print usable sub-command and return
     if len(sub) == 1:
-        say.available_cmds(log_l2)
-        return 1
+        say.available_cmds(cli.log_l2)
+        return
     else:
         l2cmd = sub.pop()   # pop out 'show'
 
     l2cmd = sub.pop()
-    if l2cmd in log_del_comp:
-        """eg: log del 20130705-110150-mj-telnet-10.0.0.1
-        """
-        print ('Permission denied')
-    elif l2cmd in log_list_comp:
+    if l2cmd in cli.log_list_comp:
         oplog.do_list()
-    elif l2cmd in log_view_comp:
+    elif l2cmd in cli.log_view_comp:
         try:
             f = sub.pop()
         except Exception:
             print("Need file name listed by \"log list\".")
-            return False
+            return
         oplog.do_view(f)
     else:
-        say.available_cmds(log_l2)
+        say.available_cmds(cli.log_l2)
 
 
 def do_help():
@@ -358,7 +227,6 @@ def do_help():
     Ctrl-R          向前搜索历史命令
     Return          把命令发给终端
     """
-    # print (do_help.__doc__)
     try:
         print string.replace(do_help.__doc__, '\n    ', '\n')
     except Exception, e:
@@ -372,23 +240,18 @@ def do_quit():
     os.exit()
 
 
-def do_shell():
-    print("Permission denied")
-    return True
-
-
 def do_show(command):
     sub = command.split()
     sub.reverse()
     # If no sub-command, print usable sub-command and return
     if len(sub) == 1:
-        say.available_cmds(show_l2)
+        say.available_cmds(cli.show_l2)
         return 1
     else:
         l2cmd = sub.pop()   # pop out 'show'
 
     l2cmd = sub.pop()
-    if l2cmd in show_history_comp:
+    if l2cmd in cli.show_history_comp:
         """show history
         """
         try:
@@ -398,15 +261,11 @@ def do_show(command):
                 print ("%s\t\b\b\b%s" % (cnt, readline.get_history_item(h)))
         except Exception, e:
             print(str(e))
-    elif l2cmd in show_my_permission_comp:
-        """show my-permission
-        """
-        say.not_implemented()
-    elif l2cmd in show_user_comp:
+    elif l2cmd in cli.show_user_comp:
         """show user
         """
         os.system('w -f')
-    elif l2cmd in show_this_server_comp:
+    elif l2cmd in cli.show_this_server_comp:
         """show this-server
         """
         print("IP address configs on this host:")
@@ -414,14 +273,12 @@ def do_show(command):
 
         print("\nRoutes on this host:")
         os.system('ip route | grep -e default -e kernel ')
-    elif l2cmd in show_time_comp:
+    elif l2cmd in cli.show_time_comp:
         """show time
         """
         os.system('cal -3; echo; date')
-    elif l2cmd is None:
-        say.available_cmds(show_l2)
     else:
-        say.available_cmds(show_l2)
+        say.available_cmds(cli.show_l2)
 
 
 def sigwinch_passthrough(sig, data):
@@ -438,19 +295,14 @@ def sigwinch_passthrough(sig, data):
 
 
 def ttywrapper():
-    # t = ANSI.term()
-
     try:
-        # l1_completer = MLCompleter(all_cmd)
-        # readline.set_completer(l1_completer.complete)
         readline.parse_and_bind('tab: complete')
         readline.set_completer_delims(' ')
     except Exception, e:
         print(str(e))
 
-    while 1:
-        readline.set_completer(MLCompleter(all_cmd).complete)
-
+    while True:
+        readline.set_completer(MLCompleter(cli.all_cmd).complete)
         global command
         # Get input, and deal with exceptions
         try:
@@ -474,33 +326,33 @@ def ttywrapper():
         command = command.strip()
         try:
             l1cmd = command.split()[0]
-            if l1cmd in clear_comp:
+            if l1cmd in cli.clear_comp:
                 do_clear()
-            elif l1cmd in help_comp:
+            elif l1cmd in cli.help_comp:
                 do_help()
-            elif l1cmd in log_comp:
+            elif l1cmd in cli.log_comp:
                 do_log(command)
-            elif l1cmd in quit_comp:
+            elif l1cmd in cli.quit_comp:
                 do_quit()
-            elif l1cmd in show_comp:
+            elif l1cmd in cli.show_comp:
                 do_show(command)
-            elif len(command.split()) >= 1 and l1cmd in log_cmd:
+            elif len(command.split()) >= 1 and l1cmd in cli.log_cmd:
                 if len(command.split()) == 1:
                     os.system(command + ' -h')
                     continue
                 run_with_log(command)
-            elif len(command.split()) >= 1 and l1cmd in nolog_cmd:
+            elif len(command.split()) >= 1 and l1cmd in cli.nolog_cmd:
                 try:
                     run_without_log(command)
                 except Exception, e:
                     print(str(e))
             else:
-                if l1cmd in all_cmd:
+                if l1cmd in cli.all_cmd:
                     say.not_implemented()
 
                 print("%sBad command:%s %s\n" %
                       (color.RED_BG, color.OFF, command))
-                say.available_cmds(sorted(all_cmd),
+                say.available_cmds(sorted(cli.all_cmd),
                                    msg="All available commands:")
         except (KeyboardInterrupt, EOFError):
             continue
