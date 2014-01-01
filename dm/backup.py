3408b4aa86f6561d51ecf2a7f0f2d20ae5f684c2
Drunkard Zhang <gongfan193@gmail.com>
2014-01-01 15:49:18 +0800

    dm: archive old files before upload new ones

--- dm/backup.py
+++ dm/backup.py
@@ -4,9 +4,30 @@ import logging
 from dm import get_host, login
 
 
+def archive_old_files(vendor):
+    """Archive previous backuped files, generally last day's."""
+    import os
+    from datetime import datetime, timedelta
+    from glob import glob
+    from etc import lazycat_conf as conf
+    logging.info('archiving old config files of: %s' % vendor)
+    p = os.path.join(conf.DEVCONF_BACKUP_PATH, vendor)
+    # Prepare archive directory
+    # lastday = "%4d%02d%02d" % time.localtime()[:3]
+    lastday = (datetime.now() - timedelta(days=1)).strftime("%Y%m%d")
+    ar_dir = os.path.join(p, lastday)
+    # Move it
+    allfile = glob(p + '/*')
+    for f in allfile:
+        fname = os.path.basename(f)
+        newf = os.path.join(ar_dir, fname)
+        os.renames(f, newf)
+
+
 def roll_on_vendor(vendor):
     from etc import dev
     hosts_set_name = 'hosts_' + vendor
+    archive_old_files(vendor)
     hosts = dev.__dict__[hosts_set_name]
     for host in sorted(hosts.keys()):
         try:
